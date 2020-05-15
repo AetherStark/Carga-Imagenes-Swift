581163c9f50bd49a4641e4c7edeef1e3b342efb2
AetherStark <fran.16980405@gmail.com>
2020-05-15 17:43:15 -0500

    Initial Commit

---
+++ Cargar Imagen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cargar Imagen
+//
+//  Created by Francisco on 15/05/20.
+//  Copyright © 2020 Francisco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
