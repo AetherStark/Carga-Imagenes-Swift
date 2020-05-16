753186da446e7845a32f7f9aadd5578ae1665b04
AetherStark <fran.16980405@gmail.com>
2020-05-15 21:15:50 -0500

    Aplicacion para muestra de imagenes

--- Cargar Imagen/ViewController.swift
+++ Cargar Imagen/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var image: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func bntCargaC(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func btnCargaG(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        image?.image = img
+        self.dismiss(animated: true, completion: nil)
+        print(img)
+    }
+    
+   /* func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let img = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
+        image?.image = img
+        self.dismiss(animated: true, completion: nil)
+    }*/
 
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
